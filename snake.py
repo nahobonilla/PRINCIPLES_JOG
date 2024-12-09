4113b382e47669dd3cd2a30ecc18c0bdd19897b7
Delmy Bonilla <bonildel001@warren.k12.in.us>
2024-12-09 08:43:03 -0500

    fixed_file

--- snake.py
+++ snake.py
@@ -1,58 +1,61 @@
-# TODO: don't forget to pip install pygame in the terminal.  Else pygame functions won't be recognized.
 import pygame, random
 
+
 # Initialize pygame
-# TODO: call pygame.init()
+pygame.init()
+
 
 # Set display window
-# TODO: create a WINDOW_WIDTH variable and assign it a value of 600
-# TODO: create a WINDOW_HEIGHT variable and assign it a value of 600
-# TODO: create a display_surface variable and assign it from pygame.display.set_mode passing
-# in a tuple of WINDOW_WIDTH and WINDOW_HEIGHT
-# TODO: call pygame.display.set_caption() passing in "~~Snake~~"
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+display_surface =  pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("~~Snake~~")
+
 
 # Set FSP and clock
-# TODO: create a FPS variable and assign it a value of 20
-# TODO: create a clock variable and assign it a value of pygame.time.Clock()
+FSP = 20
+clock = pygame.time.Clock()
+
 
 # Set game values
-# TODO: create a SNAKE_SIZE variable and assign it a value of 20
-# TODO: create a head_x variable and assign it a value of WINDOW_WIDTH // 2
-# TODO: repeat for head_y and assign it a value of WINDOW_HEIGHT // 2 + 100
-# TODO: create a snake_dx variable and assign it a value of 0
-# TODO: repeat for snake_dy
-# TODO: create a score variable and assign it a value of 0
+SNAKE_SIZE = 20
+head_x = WINDOW_WIDTH // 2
+head_y = WINDOW_HEIGHT // 2 + 100
+snake_dx = 0
+snake_dy = 0
+score = 0
+
 
 # Set colors
-# TODO: create GREEN, RED and WHITE tuples for the colors.  Use Standard RGB settings for those
-# TODO: create a DARKGREEN tuple and set to (10, 50, 10)
-# TODO: create a DARKRED tuple and set to (150, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0 ,0)
+WHITE = (255, 255, 255)
+DARKGREEN = (10, 50, 10)
+DARKRED = (150, 0, 0)
+
 
 # Set fonts
 font = pygame.font.SysFont('gabriola', 48)
 
 
 # Set text
-
 def create_text_and_rect(text, color, background_color, **locations):
     text = font.render(text, True, color, background_color)
     rect = text.get_rect()
     for location in locations.keys():
         if location == "center":
             rect.center = locations[location]
-        # TODO: add an if condition for "topleft" similar to the if condition for "center"
+        elif location == "topleft":
+            rect.topleft = locations[location]
     return text, rect
 
 
-# TODO: Here is a usage example for the rest of the text and rectangles that you'll create.
 title_text, text_rect = create_text_and_rect("~~Snake~~", GREEN, DARKRED,
                                              center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2))
 
-# TODO: unpack the tuple from create_text_and_rect into a score_text and score_rect variable
-# TODO: text is "Score: " + str(score)
-# TODO: color is GREEN
-# TODO: background_color is DARKRED
-# TODO: locations are topleft=(10, 10)
+
+create_text_and_rect, score_rect = create_text_and_rect("Score:" + str(score), GREEN, DARKRED, topleft = (10,10))
+
 
 # TODO: unpack the tuple from create_text_and_rect into a game_over_text and game_over_rect variable
 # TODO: text is "GAMEOVER"
